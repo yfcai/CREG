e66245617f56fded8d516381a19e1fc3e1fe6936
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-06-18 15:18:16 +0200

    add capture-avoiding substitution

--- experiments/CaseStudy.scala
+++ experiments/CaseStudy.scala
@@ -2,15 +2,30 @@
   *
   * Datatype generic programming with maps and folds,
   * made feasible by nominal functor declarations
+  * and equirecursive datatypes
   *
   * Usage examples:
+  *
+  *   - pretty printing visitor
   *   - prepend "_" to all variable names
   *   - compute free variables
   *   - capture-avoiding substitution
   *
   * Functors in code that should be generated:
-  *   def termF
-  *   val namesF
+  *
+  *   - termF
+  *   - namesF
+  *   - varF
+  *   - avoidF
+  *
+  * Implementation ideas of required features:
+  *
+  *   - macro system remembers structure of all datatypes
+  *     in scope to generate map/bimap/trimap etc. as
+  *     appropriate
+  *
+  *   - implicit macro tests equivalence of equirecursive
+  *     types to know where to insert safe casts/conversions
   */
 
 
@@ -22,66 +37,83 @@ trait CaseStudy {
 
   /* User writes:
 
-   sealed trait Term
-   case class Void() extends Term
-   case class Var(v: String) extends Term
-   case class Abs(x: String, body: Term) extends Term
-   case class App(f: Term, y: Term) extends Term
+   datatype Term {
+     Void()
+     Abs(x: String, body: Term)
+     App(Term, Term)
+   }
 
    */
 
   // System generates:
 
-  sealed trait TermT[V, X, B, F, Y]
-  case class Void[V, X, B, F, Y]() extends TermT[V, X, B, F, Y]
-  case class Var[V, X, B, F, Y](v: V) extends TermT[V, X, B, F, Y]
-  case class Abs[V, X, B, F, Y](x: X, body: B) extends TermT[V, X, B, F, Y]
-  case class App[V, X, B, F, Y](f: F, y   : Y) extends TermT[V, X, B, F, Y]
+  // expanded shape template for datatype Term
+
+  type ⊥ = Nothing
+
+  sealed trait TermT[+Void, +Var, +Abs, +App]
+  sealed trait Void extends TermT[Void, ⊥, ⊥, ⊥] // ill-founded inheritance
+  // case classes/objects should be final. For now they ain't, due to SI-4440.
+  case object Void extends Void
+  case class Var[+T](name: T) extends TermT[⊥, Var[T], ⊥, ⊥]
+  case class Abs[+Param, +Body](param: Param, body: Body) extends TermT[⊥, ⊥, Abs[Param, Body], ⊥]
+  case class App[+Fun, +Arg](fun: Fun, arg: Arg) extends TermT[⊥, ⊥, ⊥, App[Fun, Arg]]
+
+  def dontcare = sys error "don't care"
 
   trait Functor {
-    type Map[T]
+    // mapping between objects (scala types)
+    type Map[+T]
 
+    // mapping between morphisms
     def map[A, B]: (A => B) => Map[A] => Map[B]
 
+    // de-facto flattening projection to the free group A (i. e., Bag[A])
+    // to supports SYBP-style gmapQ
     def gfoldl[A](default: A, combine: (A, A) => A): Map[A] => A
   }
 
-  type FunctorOf[F[_]] = Functor { type Map[T] = F[T] }
+  // sugar
+  type FunctorOf[F[+_]] = Functor { type Map[T] = F[T] }
 
-  sealed trait Fix[F[_]] {
-    def unroll: F[Fix[F]]
+  // fixed point of functor, featuring ill-founded inheritance
+  sealed trait Fix[+F[+_]] { def unroll: F[Fix[F]] }
 
+  // cannot have case class Fix[F[_](unroll: Fix[F]) directly: illegal inheritance
+  case class Roll[+F[+_]](unroll: F[Fix[F]]) extends Fix[F]
+
+  // `fold` cannot be inside trait Fix, for it violates covariance.
+  implicit class Foldable[F[+_]](t: Fix[F]) {
     def fold[T](f: F[T] => T)(implicit functor: FunctorOf[F]): T =
-      f(functor.map[Fix[F], T](_.fold(f))(unroll))
+      f(functor.map[Fix[F], T](_.fold(f))(t.unroll))
   }
 
-  case class Roll[F[_]](unroll: F[Fix[F]]) extends Fix[F]
-/*
-  type TermF[V, X] = {
-    type λ[T] = TermT[V, X, T, T, T]
-  }
+  // Term as the fixed point of the functor TermF
 
-  type Term = Fix[TermF[String, String]#λ]
+  // named here, because TermF is particularly important to users
+  type TermF[+T] = TermT[Void, Var[String], Abs[String, T], App[T, T]]
+
+  type Term = Fix[TermF]
 
   // Eventually there will be constructor/destructor objects Void, Var, Abs, App.
   // For now, we use smart constructors.
-  def void(): Term = Roll[TermF[String, String]#λ](Void())
-  implicit def _var(x: String): Term = Roll[TermF[String, String]#λ](Var(x))
-  def abs(x: String, body: Term): Term = Roll[TermF[String, String]#λ](Abs(x, body))
-  def app(f: Term, y: Term): Term = Roll[TermF[String, String]#λ](App(f, y))
+  def void: Term = Roll[TermF](Void)
+  implicit def _var(x: String): Term = Roll[TermF](Var(x))
+  def abs(x: String, body: Term): Term = Roll[TermF](Abs(x, body))
+  def app(f: Term, y: Term): Term = Roll[TermF](App(f, y))
 
-  implicit def termF[V, X] = new Functor {
-    type Map[T] = TermT[V, X, T, T, T]
+  implicit val termF = new Functor {
+    type Map[+T] = TermF[T]
 
     def map[A, B]: (A => B) => Map[A] => Map[B] = f => {
-      case Void() => Void()
+      case Void => Void
       case Var(v) => Var(v)
       case Abs(x, t) => Abs(x, f(t))
       case App(t1, t2) => App(f(t1), f(t2))
     }
 
     def gfoldl[A](default: A, combine: (A, A) => A): Map[A] => A = {
-      case Void() => default
+      case Void => default
       case Var(v) => default
       case Abs(x, t) => t
       case App(t1, t2) => combine(t1, t2)
@@ -95,8 +127,8 @@ trait CaseStudy {
 
   // @return (pretty-printed-string, precedence-of-top-level-operator)
   def prettyVisitor(t: Term) = t.fold[(String, Int)] {
-    case Void() =>
-      ("void", Int.MaxValue)
+    case Void =>
+      ("()", Int.MaxValue)
 
     case Var(x) =>
       (x, Int.MaxValue)
@@ -129,21 +161,41 @@ trait CaseStudy {
   // System generates:
 
   val namesF = new Functor {
-    type Map[T] = Fix[TermF[T, T]#λ]
-
-    def map[A, B] = f => _.fold[Fix[TermF[B, B]#λ]] {
-      case Void() => Roll[TermF[B, B]#λ](Void())
-      case Var(v) => Roll[TermF[B, B]#λ](Var(f(v))) // f is called
-      case Abs(x, b) => Roll[TermF[B, B]#λ](Abs(f(x), b)) // f is called
-      case App(g, y) => Roll[TermF[B, B]#λ](App(g, y))
+    private[this] type TF[+N] = {
+      type λ[+T] = TermT[Void, Var[N], Abs[N, T], App[T, T]]
     }
 
-    def gfoldl[A](nil: A, combine: (A, A) => A) = _.fold[A] {
-      case Void() => nil
-      case Var(v) => v
-      case Abs(x, b) => combine(x, b)
-      case App(g, y) => combine(g, y)
+    private[this] implicit def tfF[N] = new Functor {
+      type Map[+T] = TF[N]#λ[T]
+      def map[A, B] = f => {
+        case Void => Void
+        case Var(v) => Var(v)
+        case Abs(x, body) => Abs(x, f(body))
+        case App(g, y) => App(f(g), f(y))
+      }
+
+      def gfoldl[A](default: A, combine: (A, A) => A) = dontcare
     }
+
+    type Map[+N] = Fix[TF[N]#λ]
+
+    // System should infer type argument of Foldable (scalac 2.11.0 can't)
+    def map[A, B] =
+      f => t => new Foldable[TF[A]#λ](t).fold[Fix[TF[B]#λ]] {
+        case Void => Roll[TF[B]#λ](Void)
+        case Var(v) => Roll[TF[B]#λ](Var(f(v))) // f is called
+        case Abs(x, b) => Roll[TF[B]#λ](Abs(f(x), b)) // f is called
+        case App(g, y) => Roll[TF[B]#λ](App(g, y))
+      }
+
+    // System should infer `new Foldable[...]`
+    def gfoldl[A](default: A, combine: (A, A) => A) =
+      t => new Foldable[TF[A]#λ](t).fold[A] {
+        case Void => default
+        case Var(v) => v
+        case Abs(x, b) => combine(x, b) // tricky!
+        case App(g, y) => combine(g, y)
+      }
   }
 
 
@@ -160,9 +212,7 @@ trait CaseStudy {
         body - x
 
       case other =>
-        // System should infer type arguments of gfoldl
-        // and keep [String, String] as the default type arguments of termF
-        termF[String, String].gfoldl[Set[String]](Set.empty, _ ++ _)(other)
+        termF.gfoldl[Set[String]](Set.empty, _ ++ _)(other)
     }
 
 
@@ -170,18 +220,161 @@ trait CaseStudy {
 
   // User writes:
 
-  // val substF = bifunctor( (V, A) => Term { Var = Var(V), Abs = A } )
+  def fresh(default: String, avoid: Set[String]): String = {
+    var index = -1
+    var y = default
+    while (avoid(y)) {
+      index += 1
+      y = default + index
+    }
+    y
+  }
+
+  // @param avoid   set of names to avoid
+  //
+  // @param alias   pending variable renamings
+  //
+  // @return        a term alpha-equivalent to renaming of t according to `alias`
+  //                containing no bound or binding occurrence of names in `avoid`
+  def avoidCapture(avoid: Set[String], alias: Map[String, String], t: Term): Term =
+    avoidF.bimap(
+      alias withDefault identity
+        ,
+      (abs: Abs[String, Term]) => {
+        val Abs(x, body) = abs
+        val y = fresh(x, avoid)
+        if (x == y)
+          Abs(x, avoidCapture(avoid, alias, body))
+        else
+          Abs(y, avoidCapture(avoid + y, alias + (x -> y), body))
+      }
+        // System should infer these casts, they are safe up to erasure.
+    )(t.asInstanceOf[avoidF.Bimap[String, Abs[String, Term]]]).asInstanceOf[Term]
+
+  // capture-avoiding substitution
+  def subst(x: String, xsub: Term, t: Term): Term =
+    avoidCapture(freevars(xsub) + x, Map.empty, t).fold[Term] {
+      case Var(y) if x == y =>
+        xsub
+
+      case other =>
+        Roll[TermF](other) // System should roll automatically
+    }
+
+  // alternative: substitution by mapping over Var
+  def subst2(x: String, xsub: Term, t: Term): Term = {
+
+    // step 1: replace String inside Var by needed Term
+    val replaced = varF.map[String, Term](
+      y => if (x == y) xsub else y
+    )(
+      // System should infer this cast
+      avoidCapture(freevars(xsub) + x, Map.empty, t).asInstanceOf[varF.Map[String]]
+    )
+
+    // step 2: remove extraneous Var constructors
+    import language.reflectiveCalls // for varF.patternFunctor
+
+    type F[V] = { type λ[+T] = TermT[Void, Var[V], Abs[String, T], App[T, T]] }
+
+    new Foldable[F[Term]#λ](replaced).fold[Term]({
+      case Var(t) =>
+        t
+
+      case other =>
+        Roll[TermF](other.asInstanceOf[TermF[Term]])
+    })(varF.patternFunctor)
+
+    // It is tempting to use
+    //
+    //   functor( V => Term { Var = V } )
+    //
+    // to do the two steps at the same time. However, the intended
+    // input morphism has type (Var => Term), and TermT[_, Term, _, _]
+    // is not an object in our category.
+  }
+
+  // val avoidF = bifunctor( (V, A) => Term { Var = Var(V), Abs = A } )
+
+  // val varF = functor( N => Term { Var(N) } )
 
   // System generates:
 
   trait Bifunctor {
-    type Bimap[A, B]
+    type Bound1 // these bounds are for replacing variants wholesale.
+    type Bound2 // we may need subtype bounds in trait Functor at some point, too.
+    type Bimap[+T1 <: Bound1, +T2 <: Bound2]
 
-    def bimap[A, B, C, D](f: A => C, g: B => D): Bimap[A, B] => Bimap[C, D]
+    def bimap[A <: Bound1, B <: Bound2, C <: Bound1, D <: Bound2](f: A => C, g: B => D):
+        Bimap[A, B] => Bimap[C, D]
   }
 
-  val substF = new Bifunctor {
-    type Bimap[A, B] = ???
+  val avoidF = new Bifunctor {
+    private[this] type F[+V, +A] = {
+      type λ[+T] = TermT[Void, Var[V], A, App[T, T]]
+    }
+
+    private implicit def fF[V, A] = new Functor {
+      type Map[+T] = F[V, A]#λ[T]
+
+      def map[A, B] = f => {
+        case Void => Void
+        case Var(v) => Var(v)
+        case Abs(x, b) => Abs(x, b)
+        case App(g, y) => App(f(g), f(y))
+      }
+
+      def gfoldl[A](default: A, combine: (A, A) => A) = dontcare
+    }
+
+    type Bound1 = Any
+    type Bound2 = Abs[_, _]
+
+    type Bimap[+V, +A <: Abs[_, _]] = Fix[F[V, A]#λ]
+
+    def bimap[A, B <: Abs[_, _], C, D <: Abs[_, _]](f: A => C, g: B => D): Bimap[A, B] => Bimap[C, D] =
+      t => new Foldable[F[A, B]#λ](t).fold[Bimap[C, D]] {
+        case Void => Roll[F[C, D]#λ](Void)
+        case Var(v) => Roll[F[C, D]#λ](Var(f(v)))
+        case abs @ Abs(_, _) => Roll[F[C, D]#λ](g(abs).asInstanceOf[F[C, D]#λ[Bimap[C, D]]])
+        case App(g, y) => Roll[F[C, D]#λ](App(g, y))
+      }
+  }
+
+  val varF = new Functor {
+    private[this] type F[+V] = {
+      type λ[+T] = TermT[Void, Var[V], Abs[String, T], App[T, T]]
+    }
+
+    implicit def patternFunctor[V] = new Functor {
+      type Map[+T] = F[V]#λ[T]
+
+      def map[A, B] = f => {
+        case Void => Void
+        case Var(v) => Var(v)
+        case Abs(x, b) => Abs(x, f(b))
+        case App(g, y) => App(f(g), f(y))
+      }
+
+      def gfoldl[A](default: A, combine: (A, A) => A) = dontcare
+    }
+
+    type Map[+V] = Fix[F[V]#λ]
+
+    def map[A, B] = f => t => new Foldable[F[A]#λ](t).fold[Map[B]] {
+      case Void => Roll[F[B]#λ](Void)
+      case Var(v) => Roll[F[B]#λ](Var(f(v))) // f called
+      case Abs(x, b) => Roll[F[B]#λ](Abs(x, b))
+      case App(g, y) => Roll[F[B]#λ](App(g, y))
+    }
+
+    def gfoldl[A](default: A, combine: (A, A) => A): Map[A] => A =
+      t => new Foldable[F[A]#λ](t).fold[A] {
+        case Void => default
+        case Var(v) => v
+        case Abs(x, b) => b
+        case App(g, y) => combine(g, y)
+      }
   }
 }
 
@@ -198,15 +391,31 @@ object CaseStudyApp extends CaseStudy with App {
   // \y -> (f x) y
   val fx_y = abs("y", app(app("f", "x"), "y"))
 
+  // \f -> f (\z -> ())
+  val fzv = abs("f", app("f", abs("z", void)))
+
   def put (name: String, obj : Any ) = println(s"$name = $obj")
   def show(name: String, term: Term) = put(name, pretty(term))
 
-  List(("id", id), ("idy", idy), ("f_xy", f_xy), ("fx_y", fx_y)).foreach {
+  List(
+    ("id", id), ("idy", idy), ("f_xy", f_xy), ("fx_y", fx_y),
+    ("fzv", fzv)
+  ).foreach {
     case (name, term) =>
       show(name, term)
       put(s"freevars($name)", freevars(term))
       show(s"prependUnderscore($name)", prependUnderscore(term))
+      List(
+        ("y", app("x", "x")),
+        ("y", app("x", "y"))
+      ).foreach {
+        case (y, ysub) =>
+          val s1 = subst(y, ysub, term)
+          val s2 = subst2(y, ysub, term)
+          assert(s1 == s2)
+          show(s"subst($y, ${pretty{ysub}}, $name)", s1)
+      }
       println()
   }
- */
+
 }
