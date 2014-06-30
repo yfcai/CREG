26cdea885114fad35e505268dfc61279c90d0934
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-06-30 15:51:05 +0200

    try to normalize generic types, with limited success

--- macro/normalize.scala
+++ macro/normalize.scala
@@ -5,10 +5,34 @@ import scala.annotation.StaticAnnotation
 object normalize {
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
-    val q"type $normalized = $synonym" = annottees.head.tree
-    val tpe = c.typecheck(q"??? : $synonym").tpe
-    val name = TermName(normalized.toString)
-    val result = q"""object $name { override def toString: String = ${tpe.dealias.toString} }"""
+    val q"type $normalized [..$params] = $rhs" = annottees.head.tree
+
+    val result: c.Tree =
+      if (params.isEmpty) {
+        // rhs is a concrete type
+        val tpe = c.typecheck(q"??? : $rhs").tpe
+        val name = TermName(normalized.toString)
+        q"""object $name { override def toString: String = ${tpe.dealias.toString} }"""
+      }
+      else {
+        // rhs may mention abstract types
+        val member = TypeName("__TYPE__")
+        val wrapper = c.typecheck(q"new { type $member[..$params] = $rhs }").tpe
+        val tpe = wrapper.decl(member).asType.toType.dealias
+        // up to (lack of) name capturing, tpe is the thing we want.
+
+        val name = TermName(normalized.toString)
+        val Name = TypeName(normalized.toString.capitalize)
+        q"""
+          object $name {
+            override def toString: String = ${tpe.toString}
+
+            type InnerType [..$params] = $tpe
+            // PROBLEMATIC: OLD TYPE REF IN tpe DOES NOT CARRY OVER HERE!
+          }
+        """
+      }
+
     c.Expr[Any](result)
   }
 }
--- test/NormalizeSpec.scala
+++ test/NormalizeSpec.scala
@@ -9,6 +9,8 @@ class NormalizeSpec extends FlatSpec {
   type S[X[_, _], Y[_], Z] = X[Z, Y[Z]]
   type T = S[Map, List, String]
 
+  type Trial[X] = Map[Int, X]
+
   "Normalizer macro" should "normalize type synonyms" in {
     @normalize type normalized = T
     val actual = normalized.toString
@@ -16,4 +18,18 @@ class NormalizeSpec extends FlatSpec {
     assert(actual == expected)
     info(s"T = S[Map, List, String] = $actual")
   }
+
+  it should "normalize type synonyms in the presence of abstract types" in {
+    @normalize type normalized[X] = Trial[X]
+    val actual = normalized.toString
+    val expected = "scala.collection.immutable.Map[Int,X]"
+    assert(actual == expected)
+    info(actual)
+  }
+
+  it should "normalize abstract synonyms to a form usable in new definitions" is (pending) /* {
+    @normalize type normalized[X] = Trial[X]
+    // does not compile yet...
+    val x: normalized.InnerType[String] = Map.empty[Int, String]
+  } */
 }
