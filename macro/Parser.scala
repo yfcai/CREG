ce955aa0bffad6d18993968f5de2b424cb4d7b45
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-07-02 19:30:56 +0200

    write parsers for fields, readers, intersection types & type vars
    todo: parser for records, with prerequisite for sequencing

--- macro/Parser.scala
+++ macro/Parser.scala
@@ -47,6 +47,10 @@ trait Parser {
   // shadow trait Parser by Parser[A]
   private[this] type Parser[A] = monad.contextReaderParser.Parser[A]
 
+  // KEYWORDS
+  final val keywordWITH : String = "WITH"
+  final val keyword_=>: : String = "=>:"
+
   // parsers must be lazy val because they're mutually recursive
   lazy val DatatypeP: Parser[Datatype] = ???
 
@@ -54,27 +58,96 @@ trait Parser {
 
   lazy val RecordP: Parser[Record] = ???
 
-  lazy val FieldP: Parser[Field] = ???
-
-  lazy val ReaderP: Parser[Reader] = ???
-
-  /*
-  lazy val IntersectP: Parser[Intersect] = new Singleton[Intersect]("(X with Y)") {
-    def parseSingleton(c: Context)(input: c.Tree): Either[(c.Position, String), (A, List[c.Tree])] = {
+  /** matching a named param against q"$lhs = $rhs" succeeds,
+    *  meaning we can match fields one by one
+    */
+  lazy val FieldP: Parser[PossiblyAnonymousField] = new Parser[PossiblyAnonymousField] {
+    def parse(c: Context)(input: c.Tree): Result[PossiblyAnonymousField, c.Position] = {
       import c.universe._
-      q"??? : $input" match {
-        case q"??? : ($lhs with $rhs)" =>
+      input match {
+        case q"$lhs = $rhs" =>
           for {
-            lres <- DatatypeP.parse(c)(List(lhs)).right
-            rres <- DatatypeP.parse(c)(List(rhs)).right
+            label <- IdentifierP.parse(c)(lhs)
+            body <- DatatypeP.parse(c)(rhs)
           }
-          yield Intersect(lres._1, rres._1)
+          yield NamedField(Field(label, body))
 
         case _ =>
-          fail(c)(input)
+          for { data <- DatatypeP.parse(c)(input) }
+          yield AnonymousField(data)
       }
     }
   }
-   */
+
+  sealed trait PossiblyAnonymousField
+  sealed case class NamedField(get: Field) extends PossiblyAnonymousField
+  sealed case class AnonymousField(get: Datatype) extends PossiblyAnonymousField
+
+  lazy val ReaderP: Parser[Reader] = new Parser[Reader] {
+    def parse(c: Context)(input: c.Tree): Result[Reader, c.Position] = {
+      import c.universe._
+      input match {
+        case q"$domain.${ keyword_=>: }($range)" =>
+          for {
+            domainRes <- TypeVarP.parse(c)(domain)
+            rangeRes <- DatatypeP.parse(c)(range)
+          }
+          yield Reader(domainRes, rangeRes)
+
+        case _ =>
+          Failure(input.pos, s"expect (X ${ keyword_=>: } Y)")
+      }
+    }
+  }
+
+  lazy val IntersectP: Parser[Intersect] = new Parser[Intersect] {
+    def parse(c: Context)(input: c.Tree): Result[Intersect, c.Position] = {
+      import c.universe._
+      input match {
+        case q"$lhs.$keywordWITH($rhs)" =>
+          for {
+            lhsRes <- DatatypeP.parse(c)(lhs)
+            rhsRes <- DatatypeP.parse(c)(rhs)
+          }
+          yield Intersect(lhsRes, rhsRes)
+
+        case _ =>
+          Failure(input.pos, s"expect (X $keywordWITH Y)")
+      }
+    }
+  }
+
+  // TypeVarP succeeds on code that can be interpreted as scala type.
+  //
+  // will branching-by-exception cause performance issues?
+  //
+  lazy val TypeVarP: Parser[TypeVar] = new Parser[TypeVar] {
+    def parse(c: Context)(input: c.Tree): Result[TypeVar, c.Position] = {
+      import c.universe._
+      try {
+        val code = input.toString
+        c.parse(s"??? : ($code)")
+        Success(TypeVar(code))
+      }
+      catch {
+        case e: reflect.macros.ParseException =>
+          Failure(input.pos, "expect scala type or type variable")
+      }
+    }
+  }
+
+  // parses a scala identifier
+  lazy val IdentifierP: Parser[String] = new Parser[String] {
+    def parse(c: Context)(input: c.Tree): Result[String, c.Position] = {
+      import c.universe._
+      input match {
+        case Ident(name) =>
+          Success(name.toString)
+
+        case _ =>
+          Failure(input.pos, "expect identifier")
+      }
+    }
+  }
 
 }
