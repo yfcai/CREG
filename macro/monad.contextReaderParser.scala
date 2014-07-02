b7d96005dbeb6d9e6753adcc5db7c5e05e7776f6
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-07-02 18:13:57 +0200

    WIP: uncapitalize file. stage II: re-add.

---
+++ macro/monad.contextReaderParser.scala
@@ -0,0 +1,56 @@
+package monad
+package contextReaderParser
+
+import scala.reflect.macros.blackbox.Context
+
+// trait Result: a variant of maybe monad
+//
+// remark: for scalac to deal with path-dependent types adequately,
+// the object whose type member is in question must be visible at
+// type level. for that reason, Failure takes the position type
+// as a type argument. The parse result w.r.t. context c will have
+// type `Result[A, c.Position]`.
+
+sealed case class Success[+A](get: A) extends Result[A, Nothing]
+sealed case class Failure[+Position](pos: Position, message: String) extends Result[Nothing, Position]
+
+sealed trait Result[+A, +Pos] {
+  // monadic return & bind
+
+  def _return[A]: A => Result[A, Pos] = a => Success(a)
+
+  // note the encoding of use-site variance of Pos
+  def >>= [B, P >: Pos] (f: A => Result[B, P]): Result[B, P] = this match {
+    case Success(a) => f(a)
+    case fail @ Failure(_, _) => fail
+  }
+
+  // syntax sugar for for-comprehension
+
+  def flatMap[B, P >: Pos](f: A => Result[B, P]): Result[B, P] = this >>= f
+  def map[B](f: A => B): Result[B, Pos] = this >>= (_return[B] compose f)
+
+  // deterministic choice
+  // note the encoding of use-site variance of A and Pos
+  def orElse [B >: A, P >: Pos](alternative: => Result[B, P]): Result[B, P] = this match {
+    case Success(_) => this
+    case Failure(_, _) => alternative
+  }
+}
+
+trait Parser[+A] {
+  self =>
+
+  // parse, or conventionally, runM
+  // unfortunately return type is path-dependent & cannot be a synonym.
+  def parse(c: Context)(input: c.Tree): Result[A, c.Position]
+
+  // sequencing <* does not make sense for tree parsers
+
+  // deterministic choice <+
+  // featuring use-site variance
+  def <+ [B >: A] (that: Parser[B]): Parser[B] = new Parser[B] {
+    def parse(c: Context)(input: c.Tree): Result[B, c.Position] =
+      self.parse(c)(input) orElse that.parse(c)(input)
+  }
+}
