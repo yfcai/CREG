67c9790b8f50c0fcb473323e10eeee514e47df2d
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-07-02 14:03:37 +0200

    parser: failed attempt
    Parsing a tree is different from parsing a token stream.
    Need a redesign.

--- macro/Parser.scala
+++ macro/Parser.scala
@@ -3,22 +3,71 @@ import scala.reflect.macros.blackbox.Context
 import DatatypeRepresentation._
 
 trait Parser {
-    /** @param datatypeDecl
-      */
-  def parse(c: Context)(datatypeDecl: c.Tree): Variant = {
-    import c.universe._
-    datatypeDecl match {
-      case q"trait $name [..$typeParams] { ..$body }" =>
-        import reflect.runtime.universe.typeOf
 
-        // TODO: parse the expression
+  // Grammar
+  // =======
+  //
+  //      Start := DataDecl
+  //             | FamilyDecl
+  //
+  //   DataDecl := trait Variant
+  //
+  // FamilyDecl := trait Name { Datatype* }
+  //
+  //   Datatype := TypeVar
+  //             | Record
+  //             | Variant
+  //             | Function
+  //             | Intersect
+  //
+  //    Variant := Name { (Record | TypeVar)* }
+  //
+  //     Record := Name ( Field* )
+  //
+  //      Field := Datatype
+  //             | Name : Datatype
+  //
+  //   Function := TypeVar => Datatype
+  //
+  //  Intersect := Datatype with Datatype
+  //
+  //
+  // Reserved names
+  // ==============
+  //
+  // __.* (all names starting with two underscores)
 
-        // dummy output to test generation
-        Variant("Empty", Many.empty)
+  private[this] type Parser[A] = monad.ContextReaderParser[A]
+  private[this] type Singleton[A] = monad.SingletonContextReaderParser[A]
 
-      case _ =>
-        sys error "incorrect usage"
+  // parsers must be lazy val because they're mutually recursive
+  lazy val DatatypeP: Parser[Datatype] = ???
+
+  lazy val VariantP: Parser[Variant] = ???
+
+  lazy val RecordP: Parser[Record] = ???
+
+  lazy val FieldP: Parser[Field] = ???
+
+  lazy val FunctionP: Parser[CovariantFunction] = ???
+
+  /*
+  lazy val IntersectP: Parser[Intersect] = new Singleton[Intersect]("(X with Y)") {
+    def parseSingleton(c: Context)(input: c.Tree): Either[(c.Position, String), (A, List[c.Tree])] = {
+      import c.universe._
+      q"??? : $input" match {
+        case q"??? : ($lhs with $rhs)" =>
+          for {
+            lres <- DatatypeP.parse(c)(List(lhs)).right
+            rres <- DatatypeP.parse(c)(List(rhs)).right
+          }
+          yield Intersect(lres._1, rres._1)
+
+        case _ =>
+          fail(c)(input)
+      }
     }
   }
+   */
 
 }
