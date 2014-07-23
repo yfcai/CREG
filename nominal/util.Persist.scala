79a8985500c0ca939c84dc48000a497d406c2064
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-07-23 12:26:57 +0200

    give static types to persistant objects

--- nominal/util.Persist.scala
+++ nominal/util.Persist.scala
@@ -9,24 +9,25 @@ package util
 import java.io._
 import org.apache.commons.codec.binary.Base64
 import scala.reflect.macros.blackbox.Context
+import scala.reflect.runtime.universe.{ TypeTag => Tag }
 
 trait Persist {
-  def persist(c: Context)(obj: Serializable): c.Tree = {
+  def persist[T <: Serializable : Tag](c: Context)(obj: T): c.Tree = {
     import c.universe._
     val bo = new ByteArrayOutputStream
     val so = new ObjectOutputStream(bo)
+    val q"??? : $tpe" = c parse s"??? : ${implicitly[Tag[T]].tpe}"
     so writeObject obj
     so.flush
     // without base64 encoding, get java.io.StreamCorruptedException due to UTF-8 encoding
     val string = Base64 encodeBase64 bo.toByteArray
-    val stringExpr = c.Expr[String](q"$string")
-    reify({
+    q"""{
       import java.io._
       import org.apache.commons.codec.binary.Base64
-      val b = Base64 decodeBase64 stringExpr.splice
+      val b = Base64 decodeBase64 $string
       val bi = new ByteArrayInputStream(b)
       val si = new ObjectInputStream(bi)
-      si.readObject
-    }).tree
+      si.readObject.asInstanceOf[$tpe]
+    }"""
   }
 }
