300b32b36ac73cf2223c3fdde2c79b252b20cbc9
Yufei Cai <cai@mathematik.uni-marburg.de>
2014-09-26 16:12:49 +0200

    outline treatment of projections by threading their info upwards

--- nominal/compiler.KIRV.scala
+++ nominal/compiler.KIRV.scala
@@ -8,6 +8,13 @@ import scala.reflect.macros.blackbox.Context
 import Functor._
 
 trait KIRV extends util.Traverse {
+  // specification of whether something is a projection or not
+  abstract class KIRV[Tree] { def get: Tree }
+  // projections choose an argument as the result
+  case class Project[Tree](i: Int, n: Int, get: Tree) extends KIRV[Tree]
+  // nestings put types under a constant heading
+  case class Nest[Tree](get: Tree) extends KIRV[Tree]
+
   /** n-nary traversable functor mapping everything to tau */
   def constF(c: Context, n: Int)(tau: Code): c.Tree = {
     import c.universe._
@@ -17,14 +24,18 @@ trait KIRV extends util.Traverse {
     }
   }
 
+  def constK(c: Context, n: Int)(tau: Code): KIRV[c.Tree] = Nest(constF(c, n)(tau))
+
   /** n-nary traversable functor returning its i-th argument */
-  def projectF(c: Context, n: Int)(i: Int): c.Tree = {
+  def projectF(c: Context, i: Int, n: Int): c.Tree = {
     import c.universe._
     newTraversableEndofunctor(c, n)(params => tq"${params(i)}") {
       case (applicative, fs, as, bs) =>
         q"${fs(i)}"
     }
   }
+
+  def projectK(c: Context, i: Int, n: Int): KIRV[c.Tree] = Project(i, n, projectF(c, i, n))
 
   /** composing functors in a record, each functor on a field */
   def compositeF(c: Context, n: Int)(record: c.Tree, fieldFs: Many[c.Tree]): c.Tree = {
@@ -46,6 +57,12 @@ trait KIRV extends util.Traverse {
         q"$recordName.traverse[..$travParams]($applicative)(..$traversals)"
     }
   }
+
+  /** composing functors with projections in mind */
+  def compositeK(c: Context, n: Int)(parent: c.Tree, children: Many[KIRV[c.Tree]]): KIRV[c.Tree] = {
+    // TODO: rectangular multiplicative multiplexing of subcategory constraints
+    ???
+  }
 }
 
 object KIRV extends KIRV {
@@ -66,7 +83,7 @@ object KIRV extends KIRV {
 
   def proj(i: Int, n: Int): Any = macro projImpl
   def projImpl(c: WhiteContext)(i: c.Expr[Int], n: c.Expr[Int]): c.Expr[Any] =
-    c.Expr(projectF(c, c eval n)(c eval i))
+    c.Expr(projectF(c, c eval i, c eval n))
 
   def composite(record: Any, n: Int)(fields: Any*): Any = macro compositeImpl
   def compositeImpl(c: WhiteContext)(record: c.Tree, n: c.Tree)(fields: c.Tree*): c.Tree =
