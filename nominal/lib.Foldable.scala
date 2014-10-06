a5fd06b380da9e864412049964d2cdb606c18bb6
Cai Yufei <cai@mathematik.uni-marburg.de>
2014-10-06 18:30:54 +0200

    revert Foldable interface
    explicit functor does not help typing because scala class
    constructor can't have path-dependent types

--- nominal/lib.Foldable.scala
+++ nominal/lib.Foldable.scala
@@ -3,8 +3,16 @@ package lib
 
 import language.higherKinds
 
-class Foldable[F[+_]](F: Traversable.EndofunctorOf[F], term: Fix[F]) {
-  def fold[T](f: F.Map[T] => T): T = {
+// constructor can't be path-dependent. this won't work:
+//
+//   class Foldable(F: Traversable.Endofunctor)(t: Fix[F.Map]) { ... }
+//
+// implicit argument gives us at least the option not to write
+//
+//   new Foldable[TermF](t)(termF)
+//
+class Foldable[F[+_]](term: Fix[F])(implicit F: Traversable.EndofunctorOf[F]) {
+  def fold[T](f: F[T] => T): T = {
     object cata extends (Fix[F] => T) {
       def apply(x: Fix[F]): T = f(F(x.unroll) map this)
     }
