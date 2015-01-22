6d51bc8c2e69e02366eb218a0a50d70c6d6c25a6
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-01-22 23:42:54 +0100

    use polymorphic variants in OCaml experiment; improve presentation

--- experiments/CaseStudy.ml
+++ experiments/CaseStudy.ml
@@ -1,12 +1,8 @@
 (* Experiment in OCaml, to take advantage of equirecursive types
  *
- * Conclusion:
- * - OCaml seems unsuitable for hosting first-class regular functors.
- *   Cai can't encode the type of `traverse` in OCaml.
- *   See `value traversable`.
- *
  * Advantage:
  * - equirecursive types out of the box
+ * - polymorphic variants
  *
  * Problem:
  * - how to write `cata`?
@@ -24,43 +20,26 @@
  *)
 
 
-type termT 'a 'b 'c 'd 'e 'f =
-  [ Lit of 'a
-  | Var of 'b
-  | Abs of ('c * 'd)
-  | App of ('e * 'f)
-  ] ;
-
-type term = termT int string string term term term;
+type term =
+  [= `Lit of int
+  |  `Var of string
+  |  `Abs of (string * term)
+  |  `App of (term * term)
+  ];
 
 (* equirecursive types out of the box! *)
 value t : term =
-  Abs "f"
-    (App
-      (App
-        (Var "f")
-        (App (Var "g") (Lit 2)))
-      (App (Var "h") (Lit 3)));
+  `Abs "f"
+    (`App
+      (`App
+        (`Var "f")
+        (`App (`Var "g") (`Lit 2)))
+      (`App (`Var "h") (`Lit 3)));
 
-(* applicative functors work rather well. *)
-
-module type Type = sig
-  type tpe;
-end;
-
-module type Map = sig
-  type map 'a;
-end;
 
 module type Functor = sig
-  include Map;
-  value map : ('a -> 'b) -> (map 'a -> map 'b);
-end;
-
-module type PureCall = sig
-  include Map;
-  value pure  : 'a -> map 'a;
-  value (<*>) : map ('a -> 'b) -> map 'a -> map 'b;
+  type map 'a;
+  value fmap : ('a -> 'b) -> (map 'a -> map 'b);
 end;
 
 module type Applicative = sig
@@ -69,115 +48,129 @@ module type Applicative = sig
   value (<*>) : map ('a -> 'b) -> map 'a -> map 'b;
 end;
 
+(* applicative functors without the obligation to define `fmap` *)
 module type PureCall = sig
-  include Map;
+  type map 'a;
   value pure  : 'a -> map 'a;
   value (<*>) : map ('a -> 'b) -> map 'a -> map 'b;
 end;
 
+(* default implementation of `fmap` in terms of `pure` and `<*>` *)
 module Applicative (PC : PureCall) = struct
   include PC;
-  value map f x = pure f <*> x;
+  value fmap f x = pure f <*> x;
 end;
 
-module type Traverse = functor (G : Applicative) -> sig
-  include Map;
-  value traverse : ('a -> G.map 'b) -> map 'a -> G.map (map 'b);
+(* Haskell's Data.Monoid *)
+module type Monoid = sig
+ type tpe;                          (* carrier set *)
+ value mempty  : tpe;               (* identity *)
+ value mappend : tpe -> tpe -> tpe; (* associative operator *)
 end;
 
-module type Trav = functor (M : Map) -> functor (G : Applicative) -> sig
-  value traverse : ('a -> G.map 'b) -> M.map 'a -> G.map (M.map 'b);
+(* examples of applicative functors *)
+module Applicative = struct
+  (* the identity functor is applicative *)
+  module Id = Applicative (struct
+    type  map 'a    = 'a;
+    value pure x    = x;
+    value (<*>) f x = f x;
+  end);
+
+  (* a constant functor into a monoid is applicative *)
+  module Const (M : Monoid) = Applicative (struct
+    open M;
+    type  map 'a    = tpe;
+    value pure x    = mempty;
+    value (<*>) f x = mappend f x;
+  end);
 end;
 
+(* traversable functors distribute over applicative functors
+ * in a weak way
+ *)
 module type Traversable = sig
   include Functor;
   module Traverse : functor (G : Applicative) -> sig
     value run : ('a -> G.map 'b) -> (map 'a -> G.map (map 'b));
+
+    (* witness that traversable functors distribute over
+     * applicative functors
+     *)
+    value seq_a : map (G.map 'a) -> G.map (map 'a);
   end;
 end;
 
+(* Traversable with neither fmap nor seq_a *)
 module type Traverse = sig
-  include Map;
+  type map 'a;
   module Traverse : functor (G : Applicative) -> sig
     value run : ('a -> G.map 'b) -> (map 'a -> G.map (map 'b));
   end;
 end;
 
-(*
-module type Traversable = functor (G : Applicative) -> sig
-  include Functor;
-  value traverse : ('a -> G.map 'b) -> map 'a -> G.map (map 'b);
-end;
-*)
-
-module type Monoid = sig
-  include Type;
-  value zero : tpe;
-  value (+)  : tpe -> tpe -> tpe;
-end;
-
-module Applicative = struct
-
-  (* repeat value `id` as submodule.
-   * does not work, either.
-   * see `value traversable`.
-   *)
-  module Id = Applicative (struct
-    type  map 'a = 'a;
-    value pure x = x;
-    value (<*>) f x = f x;
-  end);
-
-
-  module Const (M : Monoid) = Applicative (struct
-    open M;
-    type map 'a = tpe;
-    value pure x = zero;
-    value (<*>) f x = f + x;
-  end);
-end;
-
+(* default implementations of `fmap` and `seq_a` given `traverse` *)
 module Traversable (T : Traverse) = struct
   type map 'a = T.map 'a;
-  module Traverse = T.Traverse;
+
+  module Traverse (G : Applicative) = struct
+    open G;
+    include T.Traverse G;
+    value seq_a = run pure;
+  end;
 
   module TraverseId = Traverse Applicative.Id;
 
-  value map = TraverseId.run;
+  value fmap = TraverseId.run;
+
+  (* PROBLEM: cannot inline the functor application `TraverseId`
+   *          into the body of `fmap` and typecheck.
+   *)
 end;
 
-
 module TermF = Traversable (struct
-  type map 't = termT int string string 't 't 't;
+  type map 't =
+    [= `Lit of int
+    |  `Var of string
+    |  `Abs of (string * 't)
+    |  `App of ('t * 't)
+    ];
 
+  (* Traverse.run can be generated from the type constructor TermF.map *)
   module Traverse (G : Applicative) = struct
     value run (f : 'a -> G.map 'b) (t : map 'a): G.map (map 'b) =
       let open G in match t with
-      [ Lit n   -> pure (fun x -> Lit x) <*> pure n
-      | Var x   -> pure (fun x -> Var x) <*> pure x
-      | Abs x t -> pure (fun x y -> Abs x y) <*> pure x <*> f t
-      | App s t -> pure (fun x y -> App x y) <*> f s <*> f t
+      [ `Lit n   -> pure (fun x -> `Lit x) <*> pure n
+      | `Var x   -> pure (fun x -> `Var x) <*> pure x
+      | `Abs x t -> pure (fun x y -> `Abs x y) <*> pure x <*> f t
+      | `App s t -> pure (fun x y -> `App x y) <*> f s <*> f t
       ];
   end;
 end);
 
-
-(* Error: The type abbreviation fixF is cyclic
+(* PROBLEM: unable to typecheck `cata` over functors.
+ *
+ * Error: The type abbreviation fixF is cyclic
+ *
 module Cata (F : Traversable) = struct
   type fixF = F.map fixF;
   value run (algebra : F.map 'a -> 'a) (t : fixF): 'a = raise Not_found;
 end;
 *)
 
+(* given an algebra of TermF, compute the corresponding catamorphism
+ * a catamorphism is the unique morphism from the initial algebra
+ * to the given algebra
+ *)
 value rec foldTerm (algebra: TermF.map 'a -> 'a) (t: term): 'a =
-  algebra (TermF.map (foldTerm algebra) t);
+  algebra (TermF.fmap (foldTerm algebra) t);
 
 (* usage example: free variables *)
 value fv : (term -> list string) =
   foldTerm (fun x -> match x with
-    [ Lit n   -> []
-    | Var x   -> [x]
-    | Abs x t -> List.filter (fun y -> String.compare x y != 0) t
-    | App s t -> List.append s t
+    [ `Lit n   -> []
+    | `Var x   -> [x]
+    | `Abs x t -> List.filter (fun y -> String.compare x y != 0) t
+    | `App s t -> List.append s t
     ]
   );
