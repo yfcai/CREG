8c0972290e4584a0a7b649d3a8f95c5313928c14
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-03-15 15:22:13 +0100

    add missing file (Foldable.scala)

---
+++ traversableGeneration/target/scala-2.11/src_managed/test/Foldable.scala
@@ -0,0 +1,20 @@
+package creg
+
+import language.higherKinds
+
+// constructor can't be path-dependent. this won't work:
+//
+//   class Foldable(F: TraversableBounded.Endofunctor)(t: Fix[F.Map]) { ... }
+//
+// implicit argument gives us at least the option not to write
+//
+//   new Foldable[TermF](t)(termF)
+//
+class Foldable[F[+_]](term: Fix[F])(implicit F: Traversable { type Map[+A] = F[A] }) {
+  def fold[T](f: F[T] => T): T = {
+    object cata extends (Fix[F] => T) {
+      def apply(x: Fix[F]): T = f(F(x.unroll) map this)
+    }
+    cata(term)
+  }
+}
